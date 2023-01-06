3ac37d27017980383583e556c5d2c30ca06db9fe
chriskimty <christaeyoung.kim@gmail.com>
2023-01-05 23:56:53 -0500

    first commit with basic test data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [call, setCall] = useState([]);
+
+const options = {
+  method: 'POST',
+  url: 'https://api.cohere.ai/generate',
+  headers: {
+    accept: 'application/json',
+    'Cohere-Version': '2022-12-06',
+    'content-type': 'application/json',
+    authorization: 'Bearer 8kl34XlvUeLprVfyY77fq3hF5CMBUQ74X3bK9n9E'
+  },
+  data: {model: 'medium', max_tokens: 100,  return_likelihoods: 'NONE', truncate: 'END', prompt: "There once was a cat named Chabs."}
+};
+
+
+const handleFunction = (e) => {
+  e.preventDefault();
+  axios
+  .request(options)
+  .then((res) => {
+    const results = res.data.generations[0].text
+    setCall(results)
+    console.log(results)
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+  
+}  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleFunction}>click</button>
+      <h2>{ call }...</h2>
     </div>
   );
 }
