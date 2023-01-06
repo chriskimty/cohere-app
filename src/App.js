6405e1f5af6569ea5812e0d33dcd73e82d0f5c97
chriskimty <christaeyoung.kim@gmail.com>
2023-01-06 01:11:37 -0500

    testing other prompt ideas on Test component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import axios from 'axios';
 import { useState } from 'react';
+import Test from './components/Test';
 
 const App = () => {
 
   const [call, setCall] = useState([]);
-
+  const [input, setInput] = useState('');
+  // const inputValue = input.valueOf;
 const options = {
   method: 'POST',
   url: 'https://api.cohere.ai/generate',
@@ -15,28 +17,36 @@ const options = {
     'content-type': 'application/json',
     authorization: 'Bearer 8kl34XlvUeLprVfyY77fq3hF5CMBUQ74X3bK9n9E'
   },
-  data: {model: 'medium', max_tokens: 100,  return_likelihoods: 'NONE', truncate: 'END', prompt: "There once was a cat named Chabs."}
+  data: {model: 'medium', max_tokens: 100,  return_likelihoods: 'NONE', truncate: 'END', prompt: `${input}`}
 };
 
-
-const handleFunction = (e) => {
-  e.preventDefault();
+  const handleFunction = (e) => {
+    setInput(input);
+    e.preventDefault();
   axios
-  .request(options)
-  .then((res) => {
-    const results = res.data.generations[0].text
-    setCall(results)
-    console.log(results)
-  })
+    .request(options)
+    .then((res) => {
+      const results = res.data.generations[0].text;
+      // const results = res.data;
+      setCall(results);
+      console.log(results);
+    })
   .catch((error) => {
     console.error(error);
   });
-  
+    setInput('');
 }  
 
   return (
     <div className="App">
-      <button onClick={handleFunction}>click</button>
+
+      <Test />
+      <form onSubmit={handleFunction} action="submit">
+        <label htmlFor="text">type here</label>
+        <input type="text" value={input}
+          onChange={(e) => setInput(e.target.value) } required/>
+        <button>click</button>
+      </form>
       <h2>{ call }...</h2>
     </div>
   );
