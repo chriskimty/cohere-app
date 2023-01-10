adc4518ad5bd0737f7884df79c395f50bb8309f0
chriskimty <christaeyoung.kim@gmail.com>
2023-01-10 00:36:02 -0500

    added custom logo, favicon, wave background, and added some base styling, and some styling for Home and Nav components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,17 @@
+import logo from '../assets/petNameGenHorizontal.png';
+
 const Home = () => {
     return (
-        <div className="home">
-            <h1>PNG: Pet Name Generator!</h1>
-            <p>Your kittycat, puppypup, birdybird or other fur/non-fur bbs deserve a name. Use the PNG to give them their name for life!</p>
+        <div className="home wrapper">
+            <h1>
+                <span class="sr-only">Pet Name Generator</span>
+                <div className="icon">
+                    <img src={logo} alt="PNG (pet name generator) logo" />
+                </div>
+            </h1>
+            <p>There's a name every kittycat, puppypup, birdybird or other fur/non-fur bbs deserves.
+                <span>Use the PNG to give them their name for life!</span>
+            </p>
         </div>
     )
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
+import icon from '../assets/petNameGenIcon.png';
 
 const Navbar = () => {
 
   return (
-    <nav className="navbar">
-        <h1>PNG</h1>
-        <div className="links">
-          <Link to="/">Home</Link>
+    <nav className="navbar wrapper">
+      <div className="icon">
+        <Link to="/">
+          <img src={icon} alt="PNG (pet name generator) icon" />
+        </Link>
         </div>
+      <ul className="links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/examples">Examples</Link>
+        </li> 
+      </ul>
     </nav>
   );
 }
